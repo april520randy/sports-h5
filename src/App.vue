fd9d1e272f2e099b05534188359a19441691f735
unknown <april520randy@gmail.com>
2023-05-18 23:20:51 +0800

    优化登录状态逻辑描述

--- src/App.vue
+++ src/App.vue
@@ -14,9 +14,13 @@ const styles = computed(() => ({
   'padding-bottom': isShowTabbar.value ? '55px' : 0
 }))
 
-// 心跳检测 用于 记录页面开启状态下的当前时间戳
-// 心跳检测一定要延迟执行，这是因为如果应用开启就开始心跳检测
-// 会立即刷新上次保存的时间戳
+/*
+  心跳检测 用于 刷新并记录应用在运行状态下的当前时间戳
+  心跳检测一定要延迟执行，这是因为如果应用一运行就开始心跳检测,会立即刷新上次应用在关闭时所保存的时间戳
+  为了确保在访问需要登录权限的网络接口时 获取到上次应用关闭时的时间戳(确保不会被心跳检查立即刷新破坏)
+  要延迟执行的时长一定要大于 finalValue = finalValue = maxResponseTime + toleranceValue + frictionValue
+  大于 finalValue 多少则可随意，我们暂定 15s吧
+*/
 
 const logTimeStamp = () => {
   // 页面运行15s之后，开启时间戳记录
@@ -25,7 +29,7 @@ const logTimeStamp = () => {
       setPageOpenedCurrentTimestamp(new Date())
     }, 1000)
     clearTimeout(timer)
-  }, 10000)
+  }, 15*1000)
 }
 logTimeStamp()
 </script>
--- src/utils/request.js
+++ src/utils/request.js
@@ -37,37 +37,37 @@ export default service
 
 // 登录状态过期逻辑
 const executeExpiredLogic = () => {
-  // 杜绝重复执行一下代码块
+  // 杜绝重复执行以下代码块
   if (!window.hasExpiredExecuteFlag) {
     window.hasExpiredExecuteFlag = true
     /*
-      后台验证token状态过期的两种情况：
+      后台验证token状态过期的情景：
       1.长时间停留在页面未动，当用户再次回来操作时，此时要弹框提示过期
       2.在使用应用的过程中被其他设备登录挤掉了，此时要弹框提示过期
-      3.关闭应用后，过了段时间回来重新打开应用，并使用了上次存的token，此时就不该提示过期弹框
+      3.关闭应用后，过了很长时间回来重新打开应用，并使用了上次存的token，此时就不该提示过期弹框，要直接执行退出逻辑
 
-      在代码上如何界定这3中情况呢? 
+      在代码上如何界定这3种情况呢? 
       你会发现 情景1/2都是应用处于运行或者在后台运行状态，即没有关闭应用，它们需要弹框提示过期.
-      情景3是应用关闭了，再重新打开则需要弹框
-      于是问题迎刃而解，只要判断当前应用是处于一个长期运行状态还是刚打开状态即可
+      情景3是应用关闭了，过了很长时间下次重新打开时则不应该弹框提示登录过期
+      于是问题迎刃而解，只要判断当前应用是处于一个长期在运行状态还是重新开启运行状态即可
 
-      在App.vue里启动一个定时器定时存储当前时间戳 模拟 心跳检测
+      在App.vue里启动一个定时器定时刷新并存储当前时间戳 模拟 心跳检测
       假设当前应用状态属于情景1/2，则 当前时间戳 - 最近一次应用心跳时间戳 ≈ 网络接口响应时长
       假设当前应用状态属于情景3，则 当前时间戳 -  最近一次应用心跳时间戳 > 网络接口响应时长
     */
     // 获取最近一次应用心跳的时间戳
     let lastPageOpenedTimeStamp = getPageOpenedCurrentTimestamp()
-    // 当前时间
+    // 当前时间戳
     let thisTime = new Date()
-    // 最大网络接口响应时长
+    // 假设最大网络接口响应时长 10s
     let maxResponseTime = 10000
     // 容差值 考虑到心跳检测的1秒延时
     const toleranceValue = 1000
-    // 摩擦值 考虑到代码执行时长，也定个1秒吧
+    // 摩擦值 考虑到代码执行最大时长，也定个1秒吧
     const frictionValue = 1000
     // 获得最终值
     const finalValue = maxResponseTime + toleranceValue + frictionValue
-    // 判断为情景3，即应用在关闭后重新启动，可以亲切称之为longTimeNoSee
+    // 假设判断为情景3，即应用在关闭后重新启动，可以亲切称之为longTimeNoSee
     let longTimeNoSee = thisTime - lastPageOpenedTimeStamp > finalValue
     if (longTimeNoSee) {
       // 执行退出
