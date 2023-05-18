07cc233f150db41eb790086db1a4ac6518607fc1
unknown <april520randy@gmail.com>
2023-05-19 00:45:39 +0800

    优化登录状态控制

--- src/App.vue
+++ src/App.vue
@@ -15,23 +15,16 @@ const styles = computed(() => ({
 }))
 
 /*
-  心跳检测 用于 刷新并记录应用在运行状态下的当前时间戳
-  心跳检测一定要延迟执行，这是因为如果应用一运行就开始心跳检测,会立即刷新上次应用在关闭时所保存的时间戳
-  为了确保在访问需要登录权限的网络接口时 获取到上次应用关闭时的时间戳(确保不会被心跳检查立即刷新破坏)
-  要延迟执行的时长一定要大于 finalValue = finalValue = maxResponseTime + toleranceValue + frictionValue
-  大于 finalValue 多少则可随意，我们暂定 15s吧
+最大网络接口响应时长为10s, timeout设置为10s
 */
-
-const logTimeStamp = () => {
-  // 页面运行15s之后，开启时间戳记录
+const tagAppStarting = () => {
+  // 应用运行10s之后，标识应用一直处于运行状态，否则应用处于重启运行状态
   let timer = setTimeout(() => {
-    setInterval(() => {
-      setPageOpenedCurrentTimestamp(new Date())
-    }, 1000)
+    window.isAppLongRunStatus = true
     clearTimeout(timer)
-  }, 15*1000)
+  }, 10*1000)
 }
-logTimeStamp()
+tagAppStarting()
 </script>
 
 <style scoped></style>
--- src/utils/request.js
+++ src/utils/request.js
@@ -51,32 +51,16 @@ const executeExpiredLogic = () => {
       情景3是应用关闭了，过了很长时间下次重新打开时则不应该弹框提示登录过期
       于是问题迎刃而解，只要判断当前应用是处于一个长期在运行状态还是重新开启运行状态即可
 
-      在App.vue里启动一个定时器定时刷新并存储当前时间戳 模拟 心跳检测
-      假设当前应用状态属于情景1/2，则 当前时间戳 - 最近一次应用心跳时间戳 ≈ 网络接口响应时长
-      假设当前应用状态属于情景3，则 当前时间戳 -  最近一次应用心跳时间戳 > 网络接口响应时长
     */
-    // 获取最近一次应用心跳的时间戳
-    let lastPageOpenedTimeStamp = getPageOpenedCurrentTimestamp()
-    // 当前时间戳
-    let thisTime = new Date()
-    // 假设最大网络接口响应时长 10s
-    let maxResponseTime = 10000
-    // 容差值 考虑到心跳检测的1秒延时
-    const toleranceValue = 1000
-    // 摩擦值 考虑到代码执行最大时长，也定个1秒吧
-    const frictionValue = 1000
-    // 获得最终值
-    const finalValue = maxResponseTime + toleranceValue + frictionValue
-    // 假设判断为情景3，即应用在关闭后重新启动，可以亲切称之为longTimeNoSee
-    let longTimeNoSee = thisTime - lastPageOpenedTimeStamp > finalValue
-    if (longTimeNoSee) {
+   // 判断如果当前应用处于长期运行状态
+    if(window.isAppLongRunStatus){
+      // 弹框提示登录失效
+      loginStatusExpired(Dialog, store, Router)
+    }else{
       // 执行退出
       dispatch('logOut')
       // 刷新页面
       // location.reload()
-    } else {
-      // 弹框提示登录失效
-      loginStatusExpired(Dialog, store, Router)
     }
   }
 }
