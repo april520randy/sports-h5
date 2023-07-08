d55816aa1405d4c26fea5bbbabae3dfea6e41dc7
ryder <ryder@kunpeng.company>
2023-07-08 15:56:11 +0800

    压缩图片

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import { VantResolver } from 'unplugin-vue-components/resolvers'
-// import styleImport from 'vite-plugin-style-import'
+import tinify from 'tinify'
+import fs from 'fs'
+import path from 'path'
+tinify.key = 'zDScpf7z3yLMKypDs19qKDCgdC2xCbrR'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? '/sports-h5/dist/' : '',
@@ -13,7 +16,15 @@ export default defineConfig({
     Components({
       resolvers: [VantResolver()]
     }),
-  ], 
+    {
+      name: 'tinyPNG',
+      apply: 'build',
+      generateBundle() {
+        const projectDirectory = path.resolve(__dirname, './src')
+        compressImages(projectDirectory)
+      }
+    }
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
@@ -23,8 +34,37 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "./src/assets/styles/mixin.scss";`,
-      },
-    },
-  },
+        additionalData: `@import "./src/assets/styles/mixin.scss";`
+      }
+    }
+  }
 })
+
+function compressImages(directory) {
+  fs.readdir(directory, (err, files) => {
+    if (err) throw err
+
+    files.forEach((file) => {
+      const filePath = path.resolve(directory, file)
+
+      fs.stat(filePath, (err, stats) => {
+        if (err) throw err
+
+        if (stats.isFile()) {
+          // 检查文件扩展名，只压缩 .png 和 .jpg 文件
+          const extension = path.extname(filePath).toLowerCase()
+          if (extension === '.png' || extension === '.jpg' || extension === '.jpeg') {
+            tinify.fromFile(filePath).toFile(filePath, (err) => {
+              if (err) throw err
+
+              console.log(`Compressed ${file}`)
+            })
+          }
+        } else if (stats.isDirectory()) {
+          // 递归处理子目录
+          compressImages(filePath)
+        }
+      })
+    })
+  })
+}
