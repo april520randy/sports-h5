e73c88dd92414d538370261893bf94330a734aa2
ryder <ryder@kunpeng.company>
2023-07-08 14:44:22 +0800

    dev

--- src/views/UserCenterView/components/BottomLink/BottomLink.vue
+++ src/views/UserCenterView/components/BottomLink/BottomLink.vue
@@ -1,24 +1,13 @@
 <template>
   <div>
-    <div class="cell-group">
-      <van-cell-group>
-        <van-cell icon="" is-link>
-          <template #title>
-            <div class="cell-item">
-              <img src="./img/set.png" alt="" />
-              <span class="custom-title">设置</span>
-            </div>
-          </template>
-        </van-cell>
-        <van-cell icon="" is-link to="/feedback">
-          <template #title>
-            <div class="cell-item">
-              <img src="./img/option.png" alt="" />
-              <span class="custom-title">意见反馈</span>
-            </div>
-          </template>
-        </van-cell>
-      </van-cell-group>
+    <div class="list">
+      <div @click="select(item)" class="item" v-for="item in list" :key="item.title">
+        <div class="left">
+          <img :src="item.icon" alt="" />
+          <span class="custom-title">{{ item.title }}</span>
+        </div>
+        <IcoArrowRight />
+      </div>
     </div>
     <div class="btn-wrapper">
       <van-button v-if="user.isLogin" @click="confirmLogOut" type="default" block plain
@@ -31,7 +20,27 @@
 <script setup>
 import { showConfirmDialog } from 'vant'
 import { useUserStore } from '@/stores/user'
+import { useRouter } from 'vue-router'
+const router = useRouter()
 const user = useUserStore()
+
+const list = [
+  {
+    title: '设置',
+    icon: new URL('./img/set.png', import.meta.url).href,
+    path: '/settings'
+  },
+  {
+    title: '意见反馈',
+    icon: new URL('./img/option.png', import.meta.url).href,
+    path: '/feedback'
+  }
+]
+const select = (item) => {
+  if (item.path) {
+    router.push(item.path)
+  }
+}
 const confirmLogOut = () => {
   showConfirmDialog({
     title: '确定退出当前账号吗?'
@@ -47,6 +56,38 @@ const confirmLogOut = () => {
 </script>
 
 <style lang="scss" scoped>
+.list {
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.1);
+  border-radius: 8px;
+  padding: 20px 15px 0;
+  .item {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding-bottom: 20px;
+    border-bottom: 1px solid #eee;
+    margin-bottom: 20px;
+    &:last-child {
+      margin-bottom: 0;
+    }
+    .left {
+      display: flex;
+      align-items: center;
+      img {
+        width: 22px;
+        margin-right: 8px;
+      }
+      span {
+        font-size: 14px;
+        color: #000;
+      }
+    }
+    svg {
+      transform: scale(1.3);
+      fill: #999;
+    }
+  }
+}
 .cell-group {
   font-size: 14px;
   border-radius: 8px;
